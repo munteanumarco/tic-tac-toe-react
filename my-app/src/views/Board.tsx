f1b6184bffe1a86b41f62a7d73ad11921bf4d704
Marco Munteanu <marco.munteanu@tecknoworks.com>
2023-10-12 19:15:55 +0300

    generic version 1.0

--- my-app/src/views/Board.tsx
+++ my-app/src/views/Board.tsx
@@ -4,59 +4,139 @@ import Cell from "./components/cell/Cell";
 import { CellValue } from "./components/models/CellValue";
 
 const Board = () => {
-  const cells: Array<CellValue> = Array(9).fill(CellValue.EmptyCell);
+  const size = 3;
+  const cellsMatrix: Array<Array<CellValue>> = Array(size)
+    .fill(CellValue.EmptyCell)
+    .map(() => Array(size).fill(CellValue.EmptyCell));
+
   const [isGameLocked, setIsGameLocked] = useState<boolean>(false);
-  const [currentCells, setCurrentCells] = useState<Array<CellValue>>(cells);
+  const [currentCells, setCurrentCells] = useState(cellsMatrix);
   const [currentMove, setCurrentMove] = useState<CellValue>(CellValue.Xcell);
 
-  const checkGameOver = () => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        currentCells[a] &&
-        currentCells[a] === currentCells[b] &&
-        currentCells[a] === currentCells[c]
-      ) {
-        return currentCells[a];
+  const checkRows = () => {
+    for (let i = 0; i < size; i++) {
+      const firstCell = currentCells[i][0];
+      let won = true;
+      if (firstCell === CellValue.EmptyCell) {
+        won = false;
       }
+      for (let j = 1; j < size && won; j++) {
+        if (currentCells[i][j] !== firstCell) {
+          won = false;
+        }
+      }
+      if (won) {
+        return firstCell;
+      }
+    }
+    return null;
+  };
+
+  const checkColumns = () => {
+    for (let j = 0; j < size; j++) {
+      const firstCell = currentCells[0][j];
+      let won = true;
+      if (firstCell === CellValue.EmptyCell) {
+        won = false;
+      }
+      for (let i = 1; i < size && won; i++) {
+        if (currentCells[i][j] !== firstCell) {
+          won = false;
+        }
+      }
+      if (won) {
+        return firstCell;
+      }
+    }
+    return null;
+  };
+
+  const checkFirstDiagonal = () => {
+    const firstCell = currentCells[0][0];
+
+    if (firstCell === CellValue.EmptyCell) {
+      return null;
+    }
+
+    for (let i = 1; i < size; i++) {
+      if (currentCells[i][i] !== firstCell) {
+        return null;
+      }
+    }
+    return firstCell;
+  };
+
+  const checkSecondDiagonal = () => {
+    const firstCell = currentCells[0][size - 1];
+
+    if (firstCell === CellValue.EmptyCell) {
+      return null;
+    }
+
+    for (let i = 1; i < size; i++) {
+      if (currentCells[i][size - i - 1] !== firstCell) {
+        return null;
+      }
+    }
+    return firstCell;
+  };
+
+  const checkGameOver = () => {
+    let rowsResult = checkRows();
+    if (rowsResult) {
+      return rowsResult;
+    }
+
+    let columnsResult = checkColumns();
+    if (columnsResult) {
+      return columnsResult;
+    }
+
+    let firstDiagonalResult = checkFirstDiagonal();
+    if (firstDiagonalResult) {
+      return firstDiagonalResult;
+    }
+
+    let secondDiagonalResult = checkSecondDiagonal();
+    if (secondDiagonalResult) {
+      return secondDiagonalResult;
     }
     return null;
   };
 
   const startNewGame = () => {
     setIsGameLocked(false);
-    setCurrentCells(cells);
+    setCurrentCells(cellsMatrix);
     setCurrentMove(CellValue.Xcell);
   };
 
   const emptyCells = () => {
-    return currentCells.filter((cell) => cell === CellValue.EmptyCell).length;
+    let count = 0;
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
+        if (currentCells[i][j] == CellValue.EmptyCell) {
+          count++;
+        }
+      }
+    }
+    return count;
   };
 
-  const handleSetNextMove = (index: number) => {
+  const handleSetNextMove = (indexRow: number, indexColumn: number) => {
     if (isGameLocked) {
       return;
     }
+
     if (currentMove === CellValue.Xcell) {
-      currentCells[index] = CellValue.Xcell;
+      currentCells[indexRow][indexColumn] = CellValue.Xcell;
       setCurrentCells(currentCells);
       setCurrentMove(CellValue.Ycell);
     } else if (currentMove === CellValue.Ycell) {
-      currentCells[index] = CellValue.Ycell;
+      currentCells[indexRow][indexColumn] = CellValue.Ycell;
       setCurrentCells(currentCells);
       setCurrentMove(CellValue.Xcell);
     }
+
     const result = checkGameOver();
 
     if (result !== null) {
@@ -72,49 +152,17 @@ const Board = () => {
 
   return (
     <div>
-      <div className="row">
-        <Cell
-          value={currentCells[0]}
-          onCellClick={() => handleSetNextMove(0)}
-        />
-        <Cell
-          value={currentCells[1]}
-          onCellClick={() => handleSetNextMove(1)}
-        />
-        <Cell
-          value={currentCells[2]}
-          onCellClick={() => handleSetNextMove(2)}
-        />
-      </div>
-      <div className="row">
-        <Cell
-          value={currentCells[3]}
-          onCellClick={() => handleSetNextMove(3)}
-        />
-        <Cell
-          value={currentCells[4]}
-          onCellClick={() => handleSetNextMove(4)}
-        />
-        <Cell
-          value={currentCells[5]}
-          onCellClick={() => handleSetNextMove(5)}
-        />
-      </div>
-      <div className="row">
-        <Cell
-          value={currentCells[6]}
-          onCellClick={() => handleSetNextMove(6)}
-        />
-        <Cell
-          value={currentCells[7]}
-          onCellClick={() => handleSetNextMove(7)}
-        />
-        <Cell
-          value={currentCells[8]}
-          onCellClick={() => handleSetNextMove(8)}
-        />
-      </div>
-      <button onClick={() => startNewGame()}>Reset Game</button>
+      {cellsMatrix.map((row, indexRow) => (
+        <div className="row">
+          {row.map((_, indexColumn) => (
+            <Cell
+              value={currentCells[indexRow][indexColumn]}
+              onCellClick={() => handleSetNextMove(indexRow, indexColumn)}
+            />
+          ))}
+        </div>
+      ))}
+      <button onClick={() => startNewGame()}>Start new game</button>
     </div>
   );
 };
